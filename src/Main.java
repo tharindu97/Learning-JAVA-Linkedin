b8b89c164a34c93c0fec4ea60eaff2c40803396f
Tharindu97 <tharindukavishna@gmail.com>
2021-06-30 19:17:38 +0530

    data types

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] arg){
+    int studentAge = 15;
+    double studentGPA = 3.5;
+    char studentGrade = 'A';
+    boolean isFalse = false;
+    System.out.println(studentAge);
+    System.out.println(studentGPA);
+    System.out.println(studentGrade);
+    System.out.println(isFalse);
+  }
+}
