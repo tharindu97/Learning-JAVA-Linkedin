9697e410f4361bd1588dc7c0ee0916180380406f
Tharindu97 <tharindukavishna@gmail.com>
2021-07-01 22:05:44 +0530

    parameters in java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
 
-  public static void announceDeveloperTeaTime(){
-    System.out.println("Waiting for tea Time");
+  public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate){
+    double tip = tipRate * listedMealPrice;
+    double tax = taxRate * listedMealPrice;
+    double result = listedMealPrice + tip + tax;
+    System.out.println("Your total meal price is: " + result);
   }
 
   public static void main(String[] arg){
-    announceDeveloperTeaTime();
+    calculateTotalMealPrice(15, 0.2, 0.08);
+    calculateTotalMealPrice(25, 0.18, 0.08);
   }
 }
