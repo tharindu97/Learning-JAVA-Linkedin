3a62cc8f7b6da4434dab02fdbdafa369279a53d0
Tharindu97 <tharindukavishna@gmail.com>
2021-07-03 21:15:18 +0530

    classes and java

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,10 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static double yearSalary(double hoursPerWeek, double amountPerHour){
-    double weeklyPayCheck  = hoursPerWeek * amountPerHour;
-    return  weeklyPayCheck * 52;
+  public static void main(String[] arg){
+    Triangle triangleA = new Triangle(15,8, 15, 8, 10);
+    Triangle triangleB = new Triangle(3, 2.598, 3, 3, 10);
+    System.out.println(triangleA.findArea());
   }
 
-  public static void main(String[] arg){
-    Scanner input = new Scanner(System.in);
-    System.out.println("Number of hours the employee works per week: ");
-    double weekHours = input.nextDouble();
-    System.out.println("Amount of money the employee makes per hours: ");
-    double moneyHours = input.nextDouble();
-    double salary = yearSalary(weekHours, moneyHours);
-    System.out.println(salary);
-  }
 }
