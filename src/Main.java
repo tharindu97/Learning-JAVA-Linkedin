c1233fee42b8dc13891f60c7b2c0d8f3b8dec473
Tharindu97 <tharindukavishna@gmail.com>
2021-08-07 17:30:50 +0530

    object, class and method example 02

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,10 @@ public class Main {
   public static void main(String[] arg){
 
     Vehicle v1 = new Vehicle();
-    v1.model = "Toyota";
-    v1.modelYear = 2010;
-    v1.accellerate();
-    System.out.println(v1.model);
-    System.out.println(v1.modelYear);
+    v1.setModel("Tharindu");
+    v1.setModelYear(2005);
+    System.out.println(v1.getModel());
+    System.out.println(v1.getModelYear());
   }
 
 }
--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,8 +1,25 @@
 public class Vehicle {
-//    Properties -> field / data
+    //    Properties -> field / data
     String model;
     int modelYear;
-//    Behaviors -> methods
+
+    //    Behaviors -> methods
+    void setModel(String value){
+        model = value;
+    }
+
+    void setModelYear(int value){
+        modelYear = value;
+    }
+
+    String getModel(){
+        return  model;
+    }
+
+    int getModelYear(){
+        return  modelYear;
+    }
+
     void accellerate(){
         System.out.println("accellerating");
     }
