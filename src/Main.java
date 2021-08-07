0960481f6197c5fb0b9d36d5c4d3680230d4a32d
Tharindu97 <tharindukavishna@gmail.com>
2021-08-07 17:07:44 +0530

    object, class and method example 01

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main {
 
   public static void main(String[] arg){
-    StudentProfile profileOne = new StudentProfile("Tharindu", "Kavishna", "SE", 3.75, 2021);
-    StudentProfile profileTwo = new StudentProfile("Tharindu", "Kavishna", "SE", 3.75, 2021);
 
-    profileTwo.incrementExpectedGraduatedYear();
-    System.out.println(profileTwo.expectedYerToGraduationYear);
+    Vehicle v1 = new Vehicle();
+    v1.model = "Toyota";
+    v1.modelYear = 2010;
+    v1.accellerate();
+    System.out.println(v1.model);
+    System.out.println(v1.modelYear);
   }
 
 }
---
+++ src/Vehicle.java
@@ -0,0 +1,9 @@
+public class Vehicle {
+//    Properties -> field / data
+    String model;
+    int modelYear;
+//    Behaviors -> methods
+    void accellerate(){
+        System.out.println("accellerating");
+    }
+}
